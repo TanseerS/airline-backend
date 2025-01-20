f75cf59001f4a8fd41eda5b41bb294fce6ce201c
Tanseer <tanseer@skillminds.com>
2025-01-20 10:21:04 +0530

    initial setup

--- routes/v1/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {infoController} = require('../../controllers')
-
-router.get('/info',infoController.info);
-
-module.exports = router;
